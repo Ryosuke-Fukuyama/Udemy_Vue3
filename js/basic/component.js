d7ce546921333049217903b365adb6f0d5036a92
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-07-11 17:52:02 +0900

    111/115

--- js/basic/component.js
+++ js/basic/component.js
@@ -1,8 +1,34 @@
-// コンポーネント
+// コンポーネント ※ネームケーシング: ケバブケースがベター
+
+// // グローバル登録(メソッド使用)
+// const vmComponent = Vue.createApp({
+//   data: () => ({
+//   })
+// }).component('hello-component', {
+//   template: '<p>Hello!</p>'
+// }).mount('#component')
+
+// ローカル登録(オプション使用)
+const helloComponent = {
+  template: '<p>Hello!</p>'
+}
+const buttonCounter = {
+  template: '<div><span>count: </span><button v-on:click="countUp">{{ count }}</button></div>',
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    countUp: function() {
+      this.count++
+    }
+  }
+}
 
 const vmComponent = Vue.createApp({
   data: () => ({
-  })
-}).component('hello-component', {
-  template: '<p>Hello!</p>'
+  }),
+  components: {
+    'hello-component': helloComponent,
+    'button-counter': buttonCounter
+  }
 }).mount('#component')
